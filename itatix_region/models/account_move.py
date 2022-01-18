18bf50b9d51eb89d6c7f05914f7b2ad119ed88f2
adhamgarcia11 <agarcia@itatix.com>
2022-01-18 11:45:55 -0600

    Region development adjustments

--- itatix_region/models/account_move.py
+++ itatix_region/models/account_move.py
@@ -6,19 +6,21 @@ class CrmLead(models.Model):
 
     region_id = fields.Many2one(comodel_name="partner.region", string="Region",
                                 domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
-                                compute='_compute_region_id',store=True
+                                compute='_compute_region_id',store=True,inverse="_inverse_region_id"
                                 )
-
-
 
     @api.depends('invoice_origin')
     def _compute_region_id(self):
         for r in self:
             if r.invoice_origin:
                 so = self.env['sale.order'].search([('name','=',r.invoice_origin)])
-                if so:
-                    r.region_id = so.region_id.id
-                else:
-                    r.region_id = False
+                r.region_id = so.region_id.id if so else False
             else:
                 r.region_id = False
+
+    def _inverse_region_id(self):
+        for r in self:
+            if r.invoice_origin:
+                so = self.env['sale.order'].search([('name', '=', r.invoice_origin)])
+                if so:
+                    so.region_id = r.region_id.id if r.region_id else False
