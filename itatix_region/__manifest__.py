c2b378cbbb344fbc9534c2d8301305144510141c
ivanporras <ivanporraslopez@gmail.com>
2022-11-14 15:31:51 -0600

    [IMP] Itatix region

--- itatix_region/__manifest__.py
+++ itatix_region/__manifest__.py
@@ -4,13 +4,14 @@
     'description': "Custom Fields for PeruggySama",
     'summary': 'Real Cost and Reports',
     'author': "ITATIX SA DE CV",
-    "version": '14.0.0.3',
+    "version": '14.0.1.0.3',
     'depends': ['sale','sale_management','sale_enterprise','crm','account','sale_crm'],
     'data': [
         'security/ir.model.access.csv',
         'views/crm_lead_view.xml',
         'views/sale_order_view.xml',
         'views/account_move_view.xml',
+        'views/res_partner_view.xml',
     ],
     'installable': True,
 }
