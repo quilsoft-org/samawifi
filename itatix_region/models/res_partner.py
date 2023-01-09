8b43f6e37182454c223d3c5712ed6d429c67bde7
ivanporras <iporras@ikatechsolutions.com>
2023-01-09 12:05:09 -0600

    [IMP] Regiones

--- itatix_region/models/res_partner.py
+++ itatix_region/models/res_partner.py
@@ -4,5 +4,4 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    region_id = fields.Many2one(comodel_name="partner.region",string="Region",
-                                domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    region_id = fields.Many2one(comodel_name="partner.region",string="Region")
